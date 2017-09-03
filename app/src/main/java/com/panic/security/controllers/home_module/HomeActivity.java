dd2359a67d3a3f11a457e16c2c5c868c6da4231e
David Olarte <hernan940730@gmail.com>
2017-09-02 21:43:03 -0500

    Project structure changed

--- app/src/main/java/com/panic/security/controllers/home_module/HomeActivity.java
+++ app/src/main/java/com/panic/security/controllers/home_module/HomeActivity.java
@@ -1,7 +1,9 @@
-package com.panic.security;
+package com.panic.security.controllers.home_module;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.panic.security.R;
 
 public class HomeActivity extends AppCompatActivity {
 
