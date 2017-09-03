7d3570df6cf132fc7a65270c8055f092f210e193
David Olarte <hernan940730@gmail.com>
2017-09-03 01:20:16 -0500

    stablish connection to firebase database

---
+++ app/src/main/java/com/panic/security/FirebaseReferences.java
@@ -0,0 +1,17 @@
+package com.panic.security;
+
+/**
+ * Created by david on 9/3/17.
+ */
+
+public class FirebaseReferences {
+    public static String DATABASE_REFERENCE = "panic-security";
+    public static String CRIMES_REFERENCE = "crimes";
+    public static String LOCATIONS_REFERENCE = "locations";
+    public static String PROFILES_REFERENCE = "profiles";
+    public static String REPORTS_REFERENCE = "reports";
+    public static String STOLEN_OBJECTS_REFERENCE = "stolen_objects";
+    public static String USERS_REFERENCE = "users";
+
+    public static String MESSAGE_REFERENCE = "home_message";
+}
--- app/src/main/java/com/panic/security/controllers/home_module/HomeActivity.java
+++ app/src/main/java/com/panic/security/controllers/home_module/HomeActivity.java
@@ -1,15 +1,48 @@
 package com.panic.security.controllers.home_module;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.panic.security.FirebaseReferences;
 import com.panic.security.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tv = (TextView) findViewById(R.id.home_message);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(FirebaseReferences.MESSAGE_REFERENCE);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                tv.setText(R.string.server_error);
+            }
+        });
+
     }
 }
