d0a8393336c78b3cfb44d8a337d6ad2ab074eb73
Daviddager <drdagerm@gmail.com>
2017-09-05 17:25:32 -0500

    Added login Activity

--- app/src/main/java/com/panic/security/controllers/home_module/HomeActivity.java
+++ app/src/main/java/com/panic/security/controllers/home_module/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.panic.security.controllers.home_module;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -15,6 +17,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,15 +26,56 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.panic.security.FirebaseReferences;
 import com.panic.security.R;
+import com.panic.security.controllers.login_module.LoginActivity;
 import com.panic.security.models.home_module.DBRegistersGenerator;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView tv;
+    /* Authentication with FireBase */
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if( user != null ){
+                    /* User signed in */
+                    updateUI( user );
+                }else{
+                    /* Log In User */
+                    showLoginActivity();
+                }
+            }
+        };
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void showLoginActivity() {
+        Intent intent = new Intent( this, LoginActivity.class );
+        startActivity( intent );
+    }
+
+    private void updateUI(FirebaseUser user) {
         setContentView(R.layout.activity_home);
 
         //tv = (TextView) findViewById(R.id.home_message);
--- app/src/main/java/com/panic/security/controllers/login_module/LoginActivity.java
+++ app/src/main/java/com/panic/security/controllers/login_module/LoginActivity.java
@@ -1,12 +1,21 @@
 package com.panic.security.controllers.login_module;
 
-/**
- * Created by drdagerm
- */
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-import android.app.ProgressDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.panic.security.R;
 
+public class LoginActivity extends AppCompatActivity {
 
-public class LoginActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
 
 }
+
