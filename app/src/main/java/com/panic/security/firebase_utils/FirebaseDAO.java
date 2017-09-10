6bca7742f6a87a866de2d4d8080b21aa096fe8ed
Hernan Olarte <hernan940730@gmail.com>
2017-09-10 15:52:34 -0500

    Update FirebaseDAO.java

--- app/src/main/java/com/panic/security/firebase_utils/FirebaseDAO.java
+++ app/src/main/java/com/panic/security/firebase_utils/FirebaseDAO.java
@@ -5,6 +5,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.panic.security.entities.Crime;
+import com.panic.security.entities.Location;
+import com.panic.security.entities.Profile;
+import com.panic.security.entities.Report;
+import com.panic.security.entities.StolenObject;
 import com.panic.security.entities.User;
 
 /**
@@ -18,18 +23,98 @@ public class FirebaseDAO {
         database = FirebaseDatabase.getInstance();
     }
 
-    public void getUserByUID (String UID, final DataCallback<User> callback) {
-        DatabaseReference userRef = database.getReference(FirebaseReferences.USERS_REFERENCE).child(UID);
-        userRef.addValueEventListener(new ValueEventListener() {
+    public void getUserByID(String ID, final DataCallback<User> callback) {
+        DatabaseReference ref = database.getReference(FirebaseReferences.USERS_REFERENCE).child(ID);
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue (User.class);
-                callback.onDataReceive (user);
+                User entity = dataSnapshot.getValue (User.class);
+                callback.onDataReceive (entity);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
+                callback.onDataReceive (null);
+            }
+        });
+    }
 
+    public void getCrimeByID (String ID, final DataCallback<Crime> callback) {
+        DatabaseReference ref = database.getReference(FirebaseReferences.CRIMES_REFERENCE).child(ID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Crime entity = dataSnapshot.getValue (Crime.class);
+                callback.onDataReceive (entity);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onDataReceive (null);
+            }
+        });
+    }
+
+    public void getLocationByID (String ID, final DataCallback<Location> callback) {
+        DatabaseReference ref = database.getReference(FirebaseReferences.LOCATIONS_REFERENCE).child(ID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Location entity = dataSnapshot.getValue (Location.class);
+                callback.onDataReceive (entity);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onDataReceive (null);
+            }
+        });
+    }
+
+    public void getProfileByID (String ID, final DataCallback<Profile> callback) {
+        DatabaseReference ref = database.getReference(FirebaseReferences.PROFILES_REFERENCE).child(ID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Profile entity = dataSnapshot.getValue (Profile.class);
+                callback.onDataReceive (entity);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onDataReceive (null);
+            }
+        });
+    }
+
+    public void getReportByID (String ID, final DataCallback<Report> callback) {
+        DatabaseReference ref = database.getReference(FirebaseReferences.REPORTS_REFERENCE).child(ID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Report entity = dataSnapshot.getValue (Report.class);
+                callback.onDataReceive (entity);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onDataReceive (null);
+            }
+        });
+    }
+
+    public void getStolenObjectByID (String ID, final DataCallback<StolenObject> callback) {
+        DatabaseReference ref = database.getReference(FirebaseReferences.STOLEN_OBJECTS_REFERENCE).child(ID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                StolenObject entity = dataSnapshot.getValue (StolenObject.class);
+                callback.onDataReceive (entity);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onDataReceive (null);
             }
         });
     }
