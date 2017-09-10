fc7ba8863cf409b186e59a2ca16b74a5404be6b6
Hernan Olarte <hernan940730@gmail.com>
2017-09-09 19:57:38 -0500

    Fix Actionbar overlapping

--- app/src/main/java/com/panic/security/controllers/map_module/MapFragment.java
+++ app/src/main/java/com/panic/security/controllers/map_module/MapFragment.java
@@ -12,29 +12,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderApi;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.panic.security.FirebaseReferences;
 import com.panic.security.R;
 
-public class HomeFragment extends Fragment {
+public class MapFragment extends Fragment {
 
     private MapView mMapView;
     private GoogleMap googleMap;
